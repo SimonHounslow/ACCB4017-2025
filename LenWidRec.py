9b86cadc155aedb1ea754d7f47a1853b550457a6
simon <simonhounslow83@gmail.com>
2025-06-03 16:11:12 +0100

    commit

--- LenWidRec.py
+++ LenWidRec.py
@@ -1,26 +1,18 @@
 XperRow = int(input("Please enter rectangle width: "))
 #length of each line (so number of times to print "x")
-
 XRows = int(input("Enter height of rectangle: "))
 #hight of rectangle (so number of lines)
-
 Counter = 0
 #counter is now equal to the number of lines
-
 while Counter<XRows:
     #do the below when Counter is greater than 0
-
     count = 0
     #this resets the number of x on each line
-
     while count < XperRow:
-        print("x", end="")
+        print("x", end="",flush=True)
         count += 1
-
     print("")
     #normal print to finish line and move on
-
     Counter+=1
     #counter states how many rows have been printed
-
 print(f"A {XperRow}x{XRows} rectangle has been drawn")
---
+++ PrintBox.py
@@ -0,0 +1,23 @@
+
+WidthInput=int(input("Enter Width:"))
+HightInput=int(input("Enter Hight:"))
+#validation loop
+
+Wcounter = 1
+Hcounter = 1
+
+while Hcounter <= HightInput:
+    #print the line
+
+    if Hcounter == 1:
+        #first line all Xs
+
+    if Hcounter == HightInput:
+        #last line
+
+    if Hcounter != 1 and Hcounter !=HighInput:
+        #mid line with spaces
+
+    print("",end="/n")
+    Hcounter +=1
+
