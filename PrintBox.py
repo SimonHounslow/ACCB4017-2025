e0d24e0d7ff589d6c276badfd6a6b5086c53bf96
simon <simonhounslow83@gmail.com>
2025-06-03 16:16:42 +0100

    commit

--- PrintBox.py
+++ PrintBox.py
@@ -8,16 +8,12 @@ Hcounter = 1
 
 while Hcounter <= HightInput:
     #print the line
-
     if Hcounter == 1:
         #first line all Xs
-
     if Hcounter == HightInput:
         #last line
-
     if Hcounter != 1 and Hcounter !=HighInput:
         #mid line with spaces
-
     print("",end="/n")
     Hcounter +=1
 
