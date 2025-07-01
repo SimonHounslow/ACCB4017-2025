344ae5a593bc206f3c7c7625868d4f343e484a05
simon <simonhounslow83@gmail.com>
2025-07-01 16:39:34 +0100

    class

---
+++ Functions.py
@@ -0,0 +1,15 @@
+def MyFirstFuction():
+    print("We are now inside a function")
+    print("We now do as much or as little as we like")
+    num1 = int(input("Enter num1"))
+    num2 = int(input("Enter num2"))
+    print("The result is ,", num1+num2)
+    print("We are now leaving the function")
+
+
+
+print("We are now calling the function")
+
+MyFirstFuction()
+
+print("We have left the function")
